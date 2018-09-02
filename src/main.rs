752a1202cab71eb0b203859bf751cecc4f7c91ef
John Naylor <jonaylor89@gmail.com>
2018-09-02 14:18:46 -0400

    Adding opening and editor struct

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,19 @@ fn main() {
     let args = env::args().collect();
     let mut stdout = stdout().into_raw_mode().unwrap();
 
+    let editor = vint::Editor::new();
+
     if args.length >= 2 {
-        vint::editor_open(args);
+        editor.editor_open(args);
     } else {
-        vint::editor_open(None);
+        editor.editor_open(None);
     }
 
-    vint::editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
+    editor.set_status_message("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
     loop {
-        vint::editor_refresh();
-        vint::editor_process_key();
+        editor.refresh_screen();
+        editor.process_keypress();
     }
 
 }
--- src/vint.rs
+++ src/vint.rs
@@ -1,1 +1,82 @@
 
+use std::time;
+use std::io::{ File, BufReader };
+use std::fs::OpenOptions;
+
+enum editor_key {
+    BACKSPACE = 127,
+    ARROW_LEFT = 1000,
+    ARROW_RIGHT,
+    ARROW_UP,
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN,
+};
+
+struct editor_row {
+
+}
+
+struct Editor {
+    cx: i32,
+    cy: i32,
+    rx: i32,
+    rowoff: i32,
+    coloff: i32,
+    screenrows: i32,
+    screencols: i32,
+    numrows: i32,
+    row: &erow,
+    dirty: i32,
+    filename: &str,
+    statusmsg: &str,
+    statusmsg_time: &time::Instant,
+
+};
+
+impl Editor {
+    pub fn new() -> Editor {
+        let mut e = Editor {
+            cx = 0,
+            cy = 0,
+            rx = 0,
+            rowoff = 0,
+            coloff = 0,
+            numrows = 0,
+            row = None,
+            dirty = 0,
+            filename = None,
+            statusmsg = String::new(),
+            statusmsg_time = time::Instant::now(),
+        };
+
+        e.screenrows, e.screencols = termion::terminal_size().unwrap();
+
+        return e;
+    }
+
+    pub fn open(self, filename: Option<String>) {
+ 
+        if Some(filename) {
+            let file = File::open(filename)?;
+        } else {
+            let file = File::new("untitled")?;  
+        }
+
+        let mut reader = BufReader::new(file);
+        let mut line = String::new();
+        let mut linelen = file.read_line(&mut line);
+
+        while (linelen = file.read_line(&mut line)) != 0 {
+            while linelen > 0 && (line.char_at(linelen-1) == '\n' || line.char_at[linelen-1] == '\r') {
+                self.insert_row(line, linelen);
+            }
+        }
+    }
+
+    self.dirty = 0;
+}
+
