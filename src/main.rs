340a3766c5e6b7d4c982f9cdca7d7c4b9925c9de
John Naylor <jonaylor89@gmail.com>
2018-09-05 23:55:23 -0400

    Adding welcome screen

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,47 @@ impl Editor {
     fn draw_rows(&mut self, buf: &mut String) {
 
         for y in 0..self.screenrows {
-            buf.push_str("~");
+
+            if y == self.screenrows / 3 {
+                let welcome = "Vint Editor";
+
+                if welcome.len() > self.screencols as usize{
+                    let (first_str, last_str) = welcome.split_at(self.screencols as usize);
+
+                    let mut padding = (self.screencols as usize - first_str.len()) / 2;
+                    if padding != 0 {
+                        buf.push_str("~");
+                        padding -= 1;
+                    }
+
+                    while padding != 0 {
+                        padding -= 1;
+                        buf.push_str(" ");
+                    }
+
+                    buf.push_str(first_str);
+
+                } else {
+
+                    let mut padding = (self.screencols as usize - welcome.len()) / 2;
+                    if padding != 0 {
+                        buf.push_str("~");
+                        padding -= 1;
+                    }
+
+                    while padding != 0 {
+                        padding -= 1;
+                        buf.push_str(" ");
+                    }
+
+                    buf.push_str(welcome);
+                }
+            
+            } else {
+                buf.push_str("~");
+            }
+
+            buf.push_str(format!("{}", termion::clear::UntilNewline).as_str());
 
             if y < self.screenrows - 1 {
                 buf.push_str("\r\n");
@@ -39,13 +79,15 @@ impl Editor {
 
     fn refresh_screen(&mut self) {
 
-         let mut buf = String::new();
+        let mut buf = String::new();
 
-        buf.push_str(format!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1)).as_str());
+        buf.push_str(format!("{}", termion::cursor::Hide).as_str());
+        buf.push_str(format!("{}", termion::cursor::Goto(1, 1)).as_str());
 
         self.draw_rows(&mut buf);
 
-        buf.push_str(format!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1)).as_str());
+        buf.push_str(format!("{}", termion::cursor::Goto(1, 1)).as_str());
+        buf.push_str(format!("{}", termion::cursor::Show).as_str());
 
         write!(self.stdout, "{}", buf);
 
@@ -60,6 +102,7 @@ impl Editor {
             match key {
                 Key::Ctrl('q') => {
                     write!(self.stdout, "{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+                    self.stdout.flush().unwrap();
                     exit(0);
                 } ,
 
