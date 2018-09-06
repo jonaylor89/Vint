494f1077dc28f0746800e4633e70cd55e37cf6e6
John Naylor <jonaylor89@gmail.com>
2018-09-06 17:46:31 -0400

    Scrolling

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,18 @@ use termion::event::Key;
 use termion::raw::{IntoRawMode, RawTerminal};
 
 struct Editor {
-    cx: i32,
-    cy: i32,
+    cx: usize,
+    cy: usize,
     stdout: RawTerminal<Stdout>,
     screenrows: usize,
     screencols: usize,
+    numrows: i32,
+    row: Option<Row>,
+}
+
+struct Row {
+    size: i32,
+    chars: String,
 }
 
 impl Editor {
@@ -26,50 +33,80 @@ impl Editor {
             stdout: stdout,
             screenrows: ysize as usize,
             screencols: xsize as usize,
+            numrows: 0,
+            row: None,
         })
+    }
+
+    fn open(self) {
+
+        let f = io::open(self.filename).expect("File IO error");
+        let mut file = BufRead::new(&f);
+        let mut line: &mut String;
+
+        let mut linelen = io::read_line(file, line).unwrap();
+
+        while linelen > 0 && line.char_at(linelen - 1) == '\n' || line.char_at(linelen - 1) {
+            linelen -= 1; 
+        }
+
+        let row = Row {
+            size: linelen,
+            chars: line,
+        }
+
+        self.row = row;
+        self.numrows = 1;
+        
     }
 
     fn draw_rows(&mut self, buf: &mut String) {
 
         for y in 0..self.screenrows {
 
-            if y == self.screenrows / 3 {
-                let welcome = "Vint Editor";
+            if y >= self.numrows {
+                if y == self.screenrows / 3 {
+                    let welcome = "Vint Editor";
 
-                if welcome.len() > self.screencols{
-                    let (first_str, last_str) = welcome.split_at(self.screencols);
+                    if welcome.len() > self.screencols{
+                        let (first_str, last_str) = welcome.split_at(self.screencols);
 
-                    let mut padding = (self.screencols - first_str.len()) / 2;
-                    if padding != 0 {
-                        buf.push_str("~");
-                        padding -= 1;
+                        let mut padding = (self.screencols - first_str.len()) / 2;
+                        if padding != 0 {
+                            buf.push_str("~");
+                            padding -= 1;
+                        }
+
+                        while padding != 0 {
+                            padding -= 1;
+                            buf.push_str(" ");
+                        }
+
+                        buf.push_str(first_str);
+
+                    } else {
+
+                        let mut padding = (self.screencols - welcome.len()) / 2;
+                        if padding != 0 {
+                            buf.push_str("~");
+                            padding -= 1;
+                        }
+
+                        while padding != 0 {
+                            padding -= 1;
+                            buf.push_str(" ");
+                        }
+
+                        buf.push_str(welcome);
                     }
-
-                    while padding != 0 {
-                        padding -= 1;
-                        buf.push_str(" ");
-                    }
-
-                    buf.push_str(first_str);
-
-                } else {
-
-                    let mut padding = (self.screencols - welcome.len()) / 2;
-                    if padding != 0 {
-                        buf.push_str("~");
-                        padding -= 1;
-                    }
-
-                    while padding != 0 {
-                        padding -= 1;
-                        buf.push_str(" ");
-                    }
-
-                    buf.push_str(welcome);
-                }
             
+                } else {
+                    buf.push_str("~");
+                }
             } else {
-                buf.push_str("~");
+                let len = self.size;
+                if len > self.screencols {len = self.screencols;}
+                buf.push_str(self.row.chars);
             }
 
             buf.push_str(format!("{}", termion::clear::UntilNewline).as_str());
@@ -100,10 +137,30 @@ impl Editor {
 
     fn move_cursor(&mut self, key: Key) {
         match key {
-            Key::Char('a') => self.cx -= 1,
-            Key::Char('d') => self.cy += 1,
-            Key::Char('w') => self.cy -= 1,
-            Key::Char('s') => self.cy += 1,
+            Key::Up => {
+                if self.cx != 0 {
+                    self.cx -= 1;
+                }
+            },
+
+            Key::Left => {
+                if self.cy != self.screenrows - 1 {
+                    self.cy += 1;
+                }
+            },
+
+            Key::Right => {
+                if self.cy != 0 {
+                    self.cy -= 1;
+                }
+            },
+
+            Key::Down => {
+                if self.cy != self.screencols - 1 {
+                    self.cy += 1;
+                }
+            },
+
             _ => {}
         } 
     }
@@ -119,9 +176,20 @@ impl Editor {
                     write!(self.stdout, "{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
                     self.stdout.flush().unwrap();
                     exit(0);
-                } ,
+                },
 
-                Key::Char('w') | Key::Char('s') | Key::Char('a') | Key::Char('d') => self.move_cursor(key),
+                Key::Home => self.cx = 0,
+                Key::End => self.cx = self.screencols - 1,
+
+                Key::PageUp | Key::PageDown => {
+                    let mut times = self.screenrows;
+                    while times != 0 {
+                        times -= 1;
+                        self.move_cursor(if key == Key::PageUp {Key::Up} else {Key::Down});
+                    }
+                },
+
+                Key::Up | Key::Down | Key::Right | Key::Left => self.move_cursor(key),
 
                 _ => {}
             }
@@ -134,6 +202,7 @@ impl Editor {
 fn main() {
 
     let mut editor = Editor::new().unwrap();
+    editor.open();
 
     loop {
         editor.refresh_screen();
